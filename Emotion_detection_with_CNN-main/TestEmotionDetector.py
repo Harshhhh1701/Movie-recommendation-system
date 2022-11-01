1b82681f4bb3560288fa198e090ff8b820bd945f
Harsh <valambeharsh1701@gmail.com>
2022-11-01 11:22:35 +0530

    commit

--- Emotion_detection_with_CNN-main/TestEmotionDetector.py
+++ Emotion_detection_with_CNN-main/TestEmotionDetector.py
@@ -6,13 +6,13 @@ from keras.models import model_from_json
 emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
 
 # load json and create model
-json_file = open('model/emotion_model.json', 'r')
+json_file = open('D:/Emotion_detection_with_CNN-main/Emotion_detection_with_CNN-main/model/emotion_model.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 emotion_model = model_from_json(loaded_model_json)
 
 # load weights into new model
-emotion_model.load_weights("model/emotion_model.h5")
+emotion_model.load_weights("D:/Emotion_detection_with_CNN-main/Emotion_detection_with_CNN-main/model/emotion_model.h5")
 print("Loaded model from disk")
 
 # start the webcam feed
